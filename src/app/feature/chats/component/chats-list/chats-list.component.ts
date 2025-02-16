adde1551515fcbd66457ec9188d8421ffa6a9e5c
Doriantrn <71319883+DorianTrn2@users.noreply.github.com>
2025-02-16 14:57:53 +0100

    Fix/Chats (#11)
    * delete: remove uselesss file
    
    * fix: user image
    
    * feat: no chats handling

--- src/app/feature/chats/component/chats-list/chats-list.component.ts
+++ src/app/feature/chats/component/chats-list/chats-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject, Input} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {Router} from '@angular/router';
-import {APP_URL} from "src/app/core/constant/url.constant";
+import {APP_URL,BACKEND_ENDPOINT, BACKEND_URI, BACKEND_USER_ENDPOINT, USER_URL} from "src/app/core/constant/url.constant";
 import {ChatService} from "../../chats.service"
 import { ChatsMessageComponent } from '../chats-message/chats-message.component';
 import { XhrFactory } from '@angular/common';
@@ -27,7 +27,6 @@ export class ChatsListComponent {
     this.chatService.getchats()
     .subscribe({
       next: (response: any[]) =>{
-        console.log('Chats: ', response);
         this.chats = response;
         response.forEach(chat=>{
           this.loadUserImage(chat.user1.username);
@@ -40,16 +39,6 @@ export class ChatsListComponent {
     })
   }
 
-  userImage(username: string){
-    this.chatService.getuserimageurl(username)
-    .subscribe({
-      next: (response: any)=>{
-        console.log(response);
-        return response;
-      }
-    })
-  }
-
   openChat(chatId: string) {
     this.dialog.open(ChatsMessageComponent, {
       width: '600px',
@@ -58,20 +47,9 @@ export class ChatsListComponent {
   }
   
   loadUserImage(username: string) {
-    console.log("loadUserImage");
-    if (!this.userImages[username]) {
-      this.chatService.getuserimageurl(username).subscribe({
-        next: (response: any) => {
-          this.userImages[username] = response;
-        },
-        error: (error: any) => {
-          console.error('Error fetching user image:', error);
-          this.userImages[username] = 'https://via.placeholder.com/50';
-        }
-      });
+    if (!this.userImages[username]){
+      this.userImages[username] = `${BACKEND_URI}/${BACKEND_ENDPOINT.USER}/${username}/${BACKEND_USER_ENDPOINT.GET_IMAGE}`;
     }
   }
-  
-
 
 }
